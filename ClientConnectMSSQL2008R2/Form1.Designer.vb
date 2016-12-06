093cfd9f8704dc732b2c986920cd5ffbe256f904
prongbang <prongbang@gmail.com>
2016-12-06 10:52:42 +0700

    Initial commit

---
+++ ClientConnectMSSQL2008R2/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.memList = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Members"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 0
+        '
+        'memList
+        '
+        Me.memList.AutoSize = True
+        Me.memList.Location = New System.Drawing.Point(13, 103)
+        Me.memList.Name = "memList"
+        Me.memList.Size = New System.Drawing.Size(0, 13)
+        Me.memList.TabIndex = 2
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 103)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(301, 134)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(325, 248)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.memList)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "ClientConnectMSSQL2008R2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents memList As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ ClientConnectMSSQL2008R2/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim Sqlcon As SqlConnection
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim sqlCnn As SqlConnection
+        Dim sqlCmd As SqlCommand 
+        Dim connectionString = "Data Source=192.168.56.101;Initial Catalog=testdatabase;User ID=sa;Password=Ex4prongbang;"
+        Dim sql = "select * from member"
+
+        sqlCnn = New SqlConnection(connectionString)
+
+        Try
+            sqlCnn.Open()
+            Label1.Text = "Client Connected IP 192.168.56.101"
+            sqlCmd = New SqlCommand(sql, sqlCnn)
+            Dim sqlReader As SqlDataReader = sqlCmd.ExecuteReader() 
+            While sqlReader.Read()
+                ListBox1.Items.Add(sqlReader.Item(0) & "     " & sqlReader.Item(1) & "     " & sqlReader.Item(2) & "     " & sqlReader.Item(3))
+            End While
+            sqlReader.Close()
+            sqlCmd.Dispose()
+            sqlCnn.Close()
+        Catch ex As Exception
+            Label1.Text = "Can not open connection ! IP 192.168.56.101"
+        End Try
+
+        '#1.Config MSSQL on Server : http://web.synametrics.com/sqlexpressremote.htm
+        '    STEP 1: Enabling TCP/IP 
+        '    STEP 2: Configure TCP/IP   ใส้ TCP Port 1433 แทน 5171
+
+        '#2. Connect to SQL Server from client : https://stackoverflow.com/questions/10789686/how-to-connect-to-sql-server-from-client
+        '
+        '
+
+        '#VISUAL BASIC .NET tutorials : http://www.c-sharpcorner.com/1/277/visual-basic-net.aspx
+
+    End Sub
+End Class
